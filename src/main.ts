51d94e3453a7249978bca830e3aa2985af5774cb
Wellington Faustino <well.faustino@hotmail.com>
2024-09-20 23:30:16 -0300

    inicial

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+  defineCustomElements(window);
