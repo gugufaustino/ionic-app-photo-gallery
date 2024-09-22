c89fd953fe5deaa91731b9a20752a63f1e6ef255
Wellington Faustino <well.faustino@hotmail.com>
2024-09-22 04:44:59 -0300

    ajustes

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -9,6 +9,7 @@ export class ListPage {
 
   constructor() {}
   NomeUsuario = 'João';
+  NomeSeguradora = 'Liberty Seguros';
   itemList = [
     { id: '1', type: 'joao@example.com', date: '2020/01/01', completed: true },
     { id: '2', type: 'maria@example.com' ,  date: '2020/01/01', completed: true},
